5975519645a8093bfbc7e2faeaf20d6f651243c3
metaleap <philipp.schumann@gmail.com>
2013-06-11 00:58:44 +0200

    server/main.go

--- cmd/ob-server/main.go
+++ cmd/ob-server/main.go
@@ -5,15 +5,11 @@ import (
 	"fmt"
 	"os"
 
-	ugo "github.com/metaleap/go-util"
-	ustr "github.com/metaleap/go-util/str"
-
 	ob "github.com/openbase/ob-core"
 	obsrv "github.com/openbase/ob-core/server"
 )
 
 func main() {
-	var err error
 	//	command-line flags
 	dirPath := flag.String("hive", "", fmt.Sprintf("%s hive directory path to use.\nIf omitted, defaults to either current directory, or the path stored in\nthe $%s environment variable: '%s'.\n", ob.OB_TITLE, ob.ENV_OBHIVE, os.Getenv(ob.ENV_OBHIVE)))
 	addr := flag.String("addr", ":23456", "TCP address to serve HTTP requests.\nSpecify ':http' for default HTTP port or ':https' for default HTTPS port\n")
@@ -22,42 +18,6 @@ func main() {
 	logToFile := flag.Bool("log_file", false, "If false, logs to 'standard output' (console),\nif true, logs to a new log file at [hive]/log/[date-time].log\n")
 	silent := flag.Bool("silent", false, "If true, nothing is ever written to standard-output.\n")
 	flag.Parse()
-
-	//	pre-init
-	if len(*dirPath) == 0 {
-		*dirPath, _ = os.Getwd()
-		*dirPath = ob.Hive.GuessDir(*dirPath)
-	}
-	ob.Opt.Server = true
-
-	//	init
-	log := ob.NewLogger(os.Stdout)
-	if *silent {
-		log.Out = nil
-	}
-	if err = ob.Init(*dirPath, log); err != nil {
-		log.Fatal(err)
-	}
-	defer ob.Dispose()
-
-	//	create log file?
-	if *logToFile {
-		var (
-			logFile     *os.File
-			logFilePath string
-		)
-		if logFilePath, logFile, err = ob.Hive.CreateLogFile(); err != nil {
-			log.Fatal(err)
-		} else {
-			defer logFile.Close()
-			log.Infof("Log file: %s", logFilePath)
-			log.Out = logFile
-		}
-	}
-
-	//	all systems go!
-	proto := ugo.Ifs(len(*tlsCertFile) > 0 && len(*tlsKeyFile) > 0, "https", "http")
-	log.Infof("LIVE @ %s://%s%s", proto, ugo.HostName(), ustr.StripSuffix(*addr, ":"+proto))
-	obsrv.Init()
-	log.Fatal(obsrv.ListenAndServe(*addr, *tlsCertFile, *tlsKeyFile))
+	//	run until the Halting Problem is solved
+	obsrv.Main(*dirPath, *addr, *tlsCertFile, *tlsKeyFile, *logToFile, *silent)
 }
---
+++ server/main.go
@@ -0,0 +1,56 @@
+package obsrv
+
+import (
+	"os"
+
+	ugo "github.com/metaleap/go-util"
+	ustr "github.com/metaleap/go-util/str"
+
+	ob "github.com/openbase/ob-core"
+)
+
+//	Called by func main() in cmd/ob-server/main.go package.
+//	In theory, you could 'abuse' this by writing your own custom server
+//	executable instead of using ob-server, but this isn't really intended.
+//	(Do note, this function does run 'forever' and thus defers all cleanups.)
+func Main(hiveDir, httpAddr, tlsCertFile, tlsKeyFile string, logToFile, silent bool) {
+	var err error
+
+	//	pre-init
+	if len(hiveDir) == 0 {
+		hiveDir, _ = os.Getwd()
+		hiveDir = ob.Hive.GuessDir(hiveDir)
+	}
+	ob.Opt.Server = true
+
+	//	init
+	log := ob.NewLogger(os.Stdout)
+	if silent {
+		log.Out = nil
+	}
+	if err = ob.Init(hiveDir, log); err != nil {
+		log.Fatal(err)
+	}
+	defer ob.Dispose()
+
+	//	create log file?
+	if logToFile {
+		var (
+			logFile     *os.File
+			logFilePath string
+		)
+		if logFilePath, logFile, err = ob.Hive.CreateLogFile(); err != nil {
+			log.Fatal(err)
+		} else {
+			defer logFile.Close()
+			log.Infof("Log file: %s", logFilePath)
+			log.Out = logFile
+		}
+	}
+
+	//	all systems go!
+	proto := ugo.Ifs(len(tlsCertFile) > 0 && len(tlsKeyFile) > 0, "https", "http")
+	log.Infof("LIVE @ %s://%s%s", proto, ugo.HostName(), ustr.StripSuffix(httpAddr, ":"+proto))
+	Init()
+	log.Fatal(ListenAndServe(httpAddr, tlsCertFile, tlsKeyFile))
+}
